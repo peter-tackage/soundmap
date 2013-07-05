8538d7b668a0d9543678e675985ff7c04e808d91
peter-tackage <peter.tackage@gmail.com>
2013-07-05 17:21:14 +0200

    Add search interface

--- src/main/java/com/moac/android/soundmap/activity/MainActivity.java
+++ src/main/java/com/moac/android/soundmap/activity/MainActivity.java
@@ -1,15 +1,15 @@
 package com.moac.android.soundmap.activity;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.res.Configuration;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 import com.moac.android.soundmap.R;
 
 public class MainActivity extends Activity {
@@ -21,13 +21,38 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         Log.v(TAG, "onCreate()- start");
         setContentView(R.layout.activity_main);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
+
     }
 
     @Override
     public void onSaveInstanceState(Bundle _outstate) {
         super.onSaveInstanceState(_outstate);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_item_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true);
+
+        return true;
+    }
+
+    private void doSearch(String _query) {
+        Log.i(TAG, "doSearch() - query: " + _query);
     }
 
 }
